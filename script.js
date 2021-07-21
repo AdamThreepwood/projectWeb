98f75441e23e0ab065ab9523b869051c9f7709d0
JohAdams <87753284+JohAdams@users.noreply.github.com>
2021-07-21 10:48:06 +0200

    Create script.js
    //So far, only for responsive topnav...

---
+++ script.js
@@ -0,0 +1,13 @@
+//So far, only for responsive topnav...
+
+
+
+
+function homeBtn() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}

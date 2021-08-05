b2b02fbdcbb4dfee6cbb342b98db5d00f6628171
AdamThreepwood <87753284+AdamThreepwood@users.noreply.github.com>
2021-08-05 18:39:54 +0200

    Update fanfic.js

--- fanfic.js
+++ fanfic.js
@@ -20,17 +20,32 @@ let members = [
   "Jin",
 ]
 
-let randMember = members[Math.floor(Math.random() * members.length)];
-let randMember2 = members[Math.floor(Math.random() * members.length)];
-let  randAction2 = actions[Math.floor(Math.random() * actions.length)];
-let  randAction = actions[Math.floor(Math.random() * actions.length)];
-
-function randomized() {
+function createFic(){
+  function randomized() {
+    let randMember = members[Math.floor(Math.random() * members.length)];
+    let randMember2 = members[Math.floor(Math.random() * members.length)];
   return "There was a beautiful idol named " + randMember + " who loved another cute idol named " + randMember2 + ". ";
-}
-function action(){
+  }
+
+  function action(){
+    let  randAction2 = actions[Math.floor(Math.random() * actions.length)];
+    let  randAction = actions[Math.floor(Math.random() * actions.length)];
     
   return "They often " + randAction + " before they went home. In fact, before they brushed their teeth they " + randAction2 + ".";
   
+  }
+
+  let fanfic =   document.getElementById("fanfic").innerHTML = randomized("") + action("");
 }
-document.getElementById("fanfic").innerHTML = randomized("") + action("");
+
+createFic()
+
+
+
+let resetButton = document.querySelector("#resetFic")
+
+resetButton.addEventListener("click", ()=>{
+  createFic()
+})
+
+

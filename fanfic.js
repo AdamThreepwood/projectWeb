8968830d1e0b361a2e41934ffb4bab3d95dfde83
AdamThreepwood <87753284+AdamThreepwood@users.noreply.github.com>
2021-08-09 11:23:42 +0200

    Update fanfic.js

--- fanfic.js
+++ fanfic.js
@@ -7,6 +7,10 @@ let actions = [
   "baked cookies",
   "rode horses",
   "jogged to Busan",
+  "danced the night away",
+  "played the piano",
+  "took a ferry to London",
+  "sang a sad song",
   
   ]
   
@@ -18,14 +22,36 @@ let members = [
   "V",
   "Suga",
   "Jin",
+  "Sherlock",
+  "Moriarty",
+  "Shelby",
+  "Hiddleston",
+  "Hardy",
+  "Sean",
+  "Castiel",
 ]
+
+let beginnings = [
+  "Once upon a time",
+  "There once was",
+  "Long ago",
+  "In the beginning of time",
+  
+  
+  ]
+
+
+
+
+
+
 
 function createFic(){
   function randomized() {
     let randMember = members[Math.floor(Math.random() * members.length)];
     let randMember2 = members[Math.floor(Math.random() * members.length)];
-  return "There was a beautiful idol named " + randMember + " who loved another cute idol named " + randMember2 + ". ";
-  }
+  return "a beautiful idol named " + randMember + " loved another cute idol named " + randMember2 + ". ";
+}
 
   function action(){
     let  randAction2 = actions[Math.floor(Math.random() * actions.length)];
@@ -33,10 +59,17 @@ function createFic(){
     
   return "They often " + randAction + " before they went home. In fact, before they brushed their teeth they " + randAction2 + ".";
   
+}
+  function beginning(){
+    let  randBeginning = beginnings[Math.floor(Math.random() * beginnings.length)];
+    
+  return randBeginning + " ";
   }
 
-  let fanfic =   document.getElementById("fanfic").innerHTML = randomized("") + action("");
-}
+  let fanficResult =  document.getElementById("fanfic").textContent = beginning("") + randomized("") + action("");
+  fanfic.style.fontSize = "large";
+
+  }
 
 createFic()
 

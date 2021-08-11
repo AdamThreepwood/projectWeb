9a9dfdfb0c19b2eaddaee2ed285bd8714232b8d2
AdamThreepwood <87753284+AdamThreepwood@users.noreply.github.com>
2021-08-11 19:05:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 
-
-
 function homeBtn() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -9,3 +7,13 @@ function homeBtn() {
     x.className = "topnav";
   }
 }
+
+function click1() {
+        var el = document.getElementById('resetFic');
+        //alert(el.innerHTML);
+        if (el.textContent == "Make a new fiction") {
+          el.textContent = "Randomize again";
+        } else {
+          el.textContent = "Make a new fiction";
+        }
+      }
